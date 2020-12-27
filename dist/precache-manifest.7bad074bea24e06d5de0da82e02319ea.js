de3c834b356b79a8213714d24896cb0d361feb9d
scenaristeur <scenaristeur@gmail.com>
2020-12-27 23:39:00 +0100

    ajout du publicpath

---
+++ dist/precache-manifest.7bad074bea24e06d5de0da82e02319ea.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b240142511b0b779e9fb",
+    "url": "/portfolio/css/app.ba014d55.css"
+  },
+  {
+    "revision": "885e640e695088bc0462",
+    "url": "/portfolio/css/chunk-vendors.dcd8433d.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/portfolio/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "cb3fd9535f451640a4d98e13515611f1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d441e62512ab09d9176c",
+    "url": "/portfolio/js/about.ec62b292.js"
+  },
+  {
+    "revision": "b240142511b0b779e9fb",
+    "url": "/portfolio/js/app.58a82b4d.js"
+  },
+  {
+    "revision": "301e70c4d7463d465415",
+    "url": "/portfolio/js/chunk-575fc3dd.0c0c6937.js"
+  },
+  {
+    "revision": "c31a45479c883339c24a",
+    "url": "/portfolio/js/chunk-b4858ffc.d2c0d5d9.js"
+  },
+  {
+    "revision": "885e640e695088bc0462",
+    "url": "/portfolio/js/chunk-vendors.c005b721.js"
+  },
+  {
+    "revision": "1770c2524b653c4741436956a1f7f256",
+    "url": "/portfolio/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/portfolio/robots.txt"
+  }
+]);
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-const publicPath = process.env.NODE_ENV === 'production' ? '/prtfolio/' : '/'
+const publicPath = process.env.NODE_ENV === 'production' ? '/portfolio/' : '/'
 
 module.exports = {
   // options...
